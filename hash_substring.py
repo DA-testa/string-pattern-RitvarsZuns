90c43037618d3c4fc191a1609da4b20c0add15dd
RitvarsZuns <ritvars.zuns@edu.rtu.lv>
2023-04-02 21:42:28 +0300

    uzd 3.2 hash

--- hash_substring.py
+++ hash_substring.py
@@ -1,65 +1,58 @@
-# python3
+#python3
 
 def read_input():
-    input_type = input().strip().upper()
+    input_type = input().rstrip()
+
     if input_type == 'F':
         with open(f"tests/06") as f:
-            pattern = f.readline().strip()
-            text = f.readline().strip()
-    else:
-        pattern = input().strip()
-        text = input().strip()
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
+    if input_type == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
 
     return pattern, text
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    #return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    p = 1000000007 # A large prime number
-    x = 1
-    h_pattern = 0
-    h_text = 0
+    # this function should find the occurances using Rabin Karp alghoritm
+    P = len(pattern)
+    T = len(text)
+    prime = 1000000007  # A big prime number to avoid collisions
+
+# Calculate powers of prime
+    p_pow = [1]
+    for i in range(1, P):
+        p_pow.append((p_pow[i-1] * 31) % prime)
+
+# Calculate hash values for pattern and the first window of text
+    pattern_hash = 0
+    for i in range(P):
+        pattern_hash = (pattern_hash + (ord(pattern[i]) - ord('a') + 1) * p_pow[P-i-1]) % prime
+
+    text_hash = 0
+    for i in range(P):
+        text_hash = (text_hash + (ord(text[i]) - ord('a') + 1) * p_pow[P-i-1]) % prime
+
     occurrences = []
-    # Calculate the hash value of the pattern and the first window in the text
-    for i in range(len(pattern)):
-        h_pattern = (h_pattern + ord(pattern[i]) * x) % p
-        h_text = (h_text + ord(text[i]) * x) % p
-        x = (x * 263) % p
 
-# Slide the window across the text and calculate the hash value of the new window at each step
-    for i in range(len(text) - len(pattern) + 1):
-        if h_pattern == h_text:
-            if pattern == text[i:i+len(pattern)]:
+# Compare hash values and if they match, check if pattern and window are the same
+    for i in range(T - P + 1):
+        if pattern_hash == text_hash:
+            if pattern == text[i:i+P]:
                 occurrences.append(i)
-
-        if i < len(text) - len(pattern):
-        # Calculate the hash value of the next window
-            h_text = (h_text - ord(text[i]) + ord(text[i+len(pattern)]) * x) % p
+        if i < T - P:
+        # Recalculate hash value for the next window
+            text_hash = ((text_hash - (ord(text[i]) - ord('a') + 1) * p_pow[P-1]) * 31 + (ord(text[i+P]) - ord('a') + 1)) % prime
+        # If the hash value becomes negative, make it positive again
+            if text_hash < 0:
+                text_hash += prime
 
     return occurrences
 
-    # this function should find the occurances using Rabin Karp alghoritm 
-
-    # and return an iterable variable
-    #return [0]
-
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
